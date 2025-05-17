6095ef7f4d072b67f8e6927ac9fe5aa61e11dc69
Markenil Santos <togomes351@gmail.com>
2025-05-17 20:02:38 -0300

    Estruturando Pastas

---
+++ Biblioteca_Pessoal/public/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<html>
+<head>
+    <title>PHP Server Test</title>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+</head>
+<body>
+    <h1>PHP Server Test</h1>
+    <p>This is a simple PHP server test.</p> 
+</body>
+</html>"
+?>
